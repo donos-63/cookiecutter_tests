b7219243a6f15a12aaba72d169cded52329761c6
SIMON M <mathieu.simon@lojelis.com>
2021-03-19 15:10:11 +0100

    update

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -5,25 +5,25 @@ import subprocess
 
 
 #we don't use celery. keep as an example
-print("post installation processing")
+print(">>>>>>  post installation processing  <<<<<<")
 local_path = os.getcwd()
 print(local_path)
 
-print("install requirements")
+print(">>>>>>  install requirements")
 subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", os.path.join(local_path, 'requirements.txt')])
-subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", os.path.join(local_path, 'src', 'application', 'requirements.txt')])
+subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", 'requirements.txt'], cwd=os.path.join(local_path, 'src', 'application'))
 
-print("Setup db")
-subprocess.check_call(["flask","db upgrade"], cwd=os.path.join(local_path, 'src', 'application'))
+print(">>>  Setup db")
+subprocess.check_call(["flask.exe","db upgrade"], cwd=os.path.join(local_path, 'src', 'application'))
 
-print("setup application")
-subprocess.check_call(["flask","myapi init"], cwd=os.path.join(local_path, 'src', 'application'))
+print(">>>  setup application")
+subprocess.check_call(["flask.exe","myapi init"], cwd=os.path.join(local_path, 'src', 'application'))
 
-print("test installation")
+print(">>>  test installation")
 subprocess.check_call(["tox","-e", "test"], cwd=os.path.join(local_path, 'src', 'application'))
 
 
-print("post installation end")
+print(">>>>>>  post installation end  <<<<<<")
 
 """ 
 print("initialize db")
