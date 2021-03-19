2979ea6fdc081bee7a9befe8b44a5d5614503787
SIMON M <mathieu.simon@lojelis.com>
2021-03-19 16:28:16 +0100

    implement project architecture testing

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,5 @@
 import os
 import sys
-import shutil
 import subprocess
 
 
@@ -10,60 +9,20 @@ local_path = os.getcwd()
 print(local_path)
 
 print(">>>>>>  install requirements")
-subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", os.path.join(local_path, 'requirements.txt')])
-subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", 'requirements.txt'], cwd=os.path.join(local_path, 'src', 'application'))
+subprocess.run([sys.executable, "-m", "pip", "install", "-r", os.path.join(local_path, 'requirements.txt')])
+subprocess.run([sys.executable, "-m", "pip", "install", "-r", 'requirements.txt'], cwd=os.path.join(local_path, 'src', 'application'))
 
 print(">>>  Setup db")
-subprocess.run([sys.executable, "-m", "flask","db", "upgrade"], cwd=os.path.join(local_path, 'src', 'application'))
+subprocess.run([sys.executable, "-m", "flask", "db", "upgrade"], cwd=os.path.join(local_path, 'src', 'application'))
 
 print(">>>  setup application")
 subprocess.run([sys.executable, "-m", "flask","{{cookiecutter.app_name}}", "init"], cwd=os.path.join(local_path, 'src', 'application'))
+
+print(">>>  test template generation")
+subprocess.run([sys.executable, "-m", "py.test", "tests"])
 
 print(">>>  test installation")
 subprocess.run([sys.executable, "-m", "tox","-e", "test"], cwd=os.path.join(local_path, 'src', 'application'))
 
 
 print(">>>>>>  post installation end  <<<<<<")
-
-""" 
-print("initialize db")
-subprocess.check_call([sys.executable, "-m", "flask", "db upgrade", cwd=os.path.join(local_path, {{ cookiecutter.project_name }}, 'src', 'application', 'requirements.txt')])
-
-print("initialize application")
-subprocess.check_call([sys.executable, "-m", "flask", "init {{cookiecutter.app_name}}", os.path.join(local_path, {{ cookiecutter.project_name }}, 'src', 'application', 'requirements.txt')])
- """
-
-
-
-
-""" 
-use_celery = '--cookiecutter.use_celery--'
-
-
-if use_celery == "no":
-    base_path = os.getcwd()
-    app_path = os.path.join(
-        base_path,
-        '{{cookiecutter.app_name}}',
-    )
-    tasks_path = os.path.join(app_path, 'tasks')
-    celery_app_path = os.path.join(app_path, 'celery_app.py')
-
-    try:
-        shutil.rmtree(tasks_path)
-    except Exception:
-        print("ERROR: cannot delete celery tasks path %s" % tasks_path)
-        sys.exit(1)
-
-    try:
-        os.remove(celery_app_path)
-    except Exception:
-        print("ERROR: cannot delete celery application file")
-        sys.exit(1)
-
-    try:
-        os.remove(os.path.join(base_path, "tests", "test_celery.py"))
-    except Exception:
-        print("ERROR: cannot delete celery tests files")
-        sys.exit(1)
- """
