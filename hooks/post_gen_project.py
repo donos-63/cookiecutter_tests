265e6d1882821d2c4d04eae2991b745471ac358a
SIMON M <mathieu.simon@lojelis.com>
2021-03-25 12:53:02 +0100

    test deploy update

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -3,26 +3,47 @@ import sys
 import subprocess
 
 
-#we don't use celery. keep as an example
+# we don't use celery. keep as an example
 print(">>>>>>  post installation processing  <<<<<<")
 local_path = os.getcwd()
 print(local_path)
 
 print(">>>>>>  install requirements")
-subprocess.run([sys.executable, "-m", "pip", "install", "-r", os.path.join(local_path, 'requirements.txt')])
-subprocess.run([sys.executable, "-m", "pip", "install", "-r", 'requirements.txt'], cwd=os.path.join(local_path, 'src', 'application'))
-
-print(">>>  Setup db")
-subprocess.run([sys.executable, "-m", "flask", "db", "upgrade"], cwd=os.path.join(local_path, 'src', 'application'))
-
-print(">>>  setup application")
-subprocess.run([sys.executable, "-m", "flask","{{cookiecutter.app_name}}", "init"], cwd=os.path.join(local_path, 'src', 'application'))
+subprocess.run(
+    [
+        sys.executable,
+        "-m",
+        "pip",
+        "install",
+        "-r",
+        os.path.join(local_path, "requirements.txt"),
+    ]
+)
+subprocess.run(
+    [sys.executable, "-m", "pip", "install", "-r", "requirements.txt"],
+    cwd=os.path.join(local_path, "src", "application"),
+)
 
 print(">>>  test template generation")
 subprocess.run([sys.executable, "-m", "py.test", "tests"])
 
+print(">>>  Setup db")
+subprocess.run(
+    [sys.executable, "-m", "flask", "db", "upgrade"],
+    cwd=os.path.join(local_path, "src", "application"),
+)
+
+print(">>>  setup application")
+subprocess.run(
+    [sys.executable, "-m", "flask", "{{cookiecutter.app_name}}", "init"],
+    cwd=os.path.join(local_path, "src", "application"),
+)
+
 print(">>>  test installation")
-subprocess.run([sys.executable, "-m", "tox","-e", "test"], cwd=os.path.join(local_path, 'src', 'application'))
+subprocess.run(
+    [sys.executable, "-m", "tox", "-e", "test"],
+    cwd=os.path.join(local_path, "src", "application"),
+)
 
 
 print(">>>>>>  post installation end  <<<<<<")
