c9bd92db9aba7d6a9738294494f31d79d5b5964a
SIMON M <mathieu.simon@lojelis.com>
2021-03-29 17:15:05 +0200

    force UTF + CRLF

--- {{ cookiecutter.project_name }}/src/application/tests/factories.py
+++ {{ cookiecutter.project_name }}/src/application/tests/factories.py
@@ -3,7 +3,6 @@ from {{cookiecutter.app_name}}.models import User
 
 
 class UserFactory(factory.Factory):
-
     username = factory.Sequence(lambda n: "user%d" % n)
     email = factory.Sequence(lambda n: "user%d@mail.com" % n)
     password = "mypwd"
